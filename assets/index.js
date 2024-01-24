ed402ccc4af4970d622964273bbebeaa15b6c73d
Elijah Oquendo <epoquendo2@gmail.com>
2024-01-23 23:05:40 -0500

    added JS code to index.JS

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".quarterly-menu").fadeIn(300);
+  });
+  
+  $(document).ready(function () {
+    $("#logoimage").fadeIn(1000);
+  });
+  
+  $(document).ready(function () {
+    $(".location").fadeIn(1000);
+  });
+  
+  $(document).ready(function () {
+    $(".mathematicians").fadeIn(1000);
+  });
+  
